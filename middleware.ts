3b48debec2a4a12c7b0fbdf2a1272d7bbcec850e
“Haberkon” <agustin@haberkon.com>
2025-09-25 23:22:50 -0300

    Primera version de login funcional con middleware simple 2

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  return NextResponse.next()
+  return NextResponse.next();
 }
